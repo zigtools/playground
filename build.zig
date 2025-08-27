227bcd3775baeb60cbf2f1ffb1ad0566516a64fc
Techatrix <techatrix@mailbox.org>
2025-08-27 23:04:47 +0200

    rename src/main.zig to src/zls.zig

--- build.zig
+++ build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "zls-playground",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = b.path("src/zls.zig"),
             .target = target,
             .optimize = optimize,
             .imports = &.{
